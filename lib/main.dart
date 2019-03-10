29127bc78dceeed4fd91b27fce07c8f328803cac
Uğur Kazdal <thekazdal@ugurs-air.local>
2019-03-10 19:32:51 +0300

    syntax fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:music_player/theme.dart';
 import 'package:music_player/songs.dart';
 import 'dart:math';
 import 'package:music_player/bottom_controls.dart';
+import 'package:meta/meta.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -100,11 +102,13 @@ class _MyHomePageState extends State<MyHomePage> {
               child: new Container(
                 width: 125.0,
                 height: 125.0,
-                child: new ClipOval(
-                  clipper: new CircleClipper(),
-                  child: new Image.network(
-                    demoPlaylist.songs[0].albumArtUrl,
-                    fit: BoxFit.cover,
+                child: RadialSeekBar(
+                  child: new ClipOval(
+                    clipper: new CircleClipper(),
+                    child: new Image.network(
+                      demoPlaylist.songs[0].albumArtUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
@@ -119,4 +123,99 @@ class _MyHomePageState extends State<MyHomePage> {
         // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
+}
+
+
+class RadialSeekBar extends StatefulWidget {
+
+  final double trackWidth;
+  final Color trackColor;
+  final double progressWidth;
+  final Color progressColor;
+  final double progressPercent;
+  final double thumbSize;
+  final Color thumbColor;
+  final double thumbPosition;
+  final Widget child;
+
+  RadialSeekBar({
+    this.trackWidth = 3.0,
+    this.trackColor = Colors.grey,
+    this.progressWidth = 5.0,
+    this.progressColor = Colors.black,
+    this.thumbSize = 10.0,
+    this.thumbColor = Colors.black,
+    this.thumbPosition = 0.0,
+    this.child, 
+    this.progressPercent = 0.0, t
+    
+  });
+  @override
+  _RadialSeekBarState createState() => _RadialSeekBarState();
+}
+
+class _RadialSeekBarState extends State<RadialSeekBar> {
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      painter: new RadialSeekBarPainter(
+        trackWidth: widget.trackWidth,
+        trackColor: widget.trackColor,
+        progressWidth: widget.progressWidth,
+        progressColor: widget.progressColor,
+        progressPercent: widget.progressPercent,
+        thumbSize: widget.thumbSize,
+        thumbColor: widget.thumbColor,
+        thumbPosition: widget.thumbPosition,
+        
+      ),
+      child: widget.child
+    );
+  }
+}
+
+class RadialSeekBarPainter extends CustomPainter{
+
+  final double trackWidth;
+  final Paint trackPaint;
+  final double progressWidth;
+  final Paint progressPaint;
+  final double progressPercent;
+  final double thumbSize;
+  final Paint thumbPaint;
+  final double thumbPosition;
+
+  RadialSeekBarPainter({
+    @required this.trackWidth,
+    @required trackColor,
+    @required this.progressWidth,
+    @required progressColor,
+    @required this.progressPercent,
+    @required this.thumbSize,
+    @required thumbColor,
+    @required this.thumbPosition,
+  }) : trackPaint = new Paint()
+        ..color = trackColor
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = trackWidth,
+       progressPaint = new Paint()
+        ..color = progressColor
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = progressWidth
+        ..strokeCap = StrokeCap.round,
+       thumbPaint = new Paint()
+        ..color = thumbColor
+        ..style = PaintingStyle.fill;
+        
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+  
 }
